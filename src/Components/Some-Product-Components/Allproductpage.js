9d8dc76bb158f8f97be2d8eba5bcbc978dfa4229
impsonugupta01 <kumarsonu94047@gmail.com>
2024-07-17 23:26:38 +0530

    changes

--- src/Components/Some-Product-Components/Allproductpage.js
+++ src/Components/Some-Product-Components/Allproductpage.js
@@ -1,94 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../Navbar'
-import './Allproductpage.css'
-import Productcontainer from './Productcontainer'
-import {
-    collection,
-    getDocs
-} from "firebase/firestore";
-import { db } from "../../FirebaseConfigs/firebaseConfigs";
-
-const Allproductpage = (props) => {
-    // window.location.reload(false);
-
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const getProducts = () => {
-
-            const productsArray = [];
-            const path = `products-${props.type.toUpperCase()}`
-            // console.log(props)
-
-            getDocs(collection(db, path)).then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // console.log(doc.id, " => ", doc.data());
-                    productsArray.push({ ...doc.data(), id: doc.id })
-                });
-                setProducts(productsArray)
-                // console.log('done')
-            }).catch('Error error error')
-        }
-
-        getProducts();
-    }, [])
-
-
-    return (
-        <div className='allproductpage'>
-            <Navbar />
-            <div className='heading'>
-                <p>Top Results For {props.type}</p>
-            </div>
-
-            <div className="allproductcontainer">
-                {products.map((product) => (
-                    <Productcontainer
-                        key={product.id}
-                        product={product}
-                    />
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default Allproductpage
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import Navbar from '../Navbar';
-// import './Allproductpage.css';
-// import Productcontainer from './Productcontainer';
-// import { collection, getDocs } from "firebase/firestore";
+// import React, { useState, useEffect } from 'react'
+// import Navbar from '../Navbar'
+// import './Allproductpage.css'
+// import Productcontainer from './Productcontainer'
+// import {
+//     collection,
+//     getDocs
+// } from "firebase/firestore";
 // import { db } from "../../FirebaseConfigs/firebaseConfigs";
 
-// const Allproductpage = ({ type }) => {
-//     const [products, setProducts] = useState([]);
+// const Allproductpage = (props) => {
+//     // window.location.reload(false);
 
+//     const [products, setProducts] = useState([]);
 //     useEffect(() => {
 //         const getProducts = () => {
+
 //             const productsArray = [];
-//             const path = `products-${type.toUpperCase()}`;
-            
+//             const path = `products-${props.type.toUpperCase()}`
+//             // console.log(props)
+
 //             getDocs(collection(db, path)).then((querySnapshot) => {
 //                 querySnapshot.forEach((doc) => {
-//                     productsArray.push({ ...doc.data(), id: doc.id });
+//                     // console.log(doc.id, " => ", doc.data());
+//                     productsArray.push({ ...doc.data(), id: doc.id })
 //                 });
-//                 setProducts(productsArray);
-//             }).catch((error) => {
-//                 console.error("Error fetching products: ", error);
-//             });
-//         };
+//                 setProducts(productsArray)
+//                 // console.log('done')
+//             }).catch('Error error error')
+//         }
 
 //         getProducts();
-//     }, [type]);
+//     }, [])
+
 
 //     return (
 //         <div className='allproductpage'>
 //             <Navbar />
 //             <div className='heading'>
-//                 <p>Top Results For {type}</p>
+//                 <p>Top Results For {props.type}</p>
 //             </div>
 
 //             <div className="allproductcontainer">
@@ -100,7 +49,58 @@ export default Allproductpage
 //                 ))}
 //             </div>
 //         </div>
-//     );
-// };
+//     )
+// }
 
-// export default Allproductpage;
+// export default Allproductpage
+
+
+
+import React, { useState, useEffect } from 'react';
+import Navbar from '../Navbar';
+import './Allproductpage.css';
+import Productcontainer from './Productcontainer';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../FirebaseConfigs/firebaseConfigs";
+
+const Allproductpage = ({ type }) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = () => {
+            const productsArray = [];
+            const path = `products-${type.toUpperCase()}`;
+            
+            getDocs(collection(db, path)).then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    productsArray.push({ ...doc.data(), id: doc.id });
+                });
+                setProducts(productsArray);
+            }).catch((error) => {
+                console.error("Error fetching products: ", error);
+            });
+        };
+
+        getProducts();
+    }, [type]);
+
+    return (
+        <div className='allproductpage'>
+            <Navbar />
+            <div className='heading'>
+                <p>Top Results For {type}</p>
+            </div>
+
+            <div className="allproductcontainer">
+                {products.map((product) => (
+                    <Productcontainer
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Allproductpage;
